9b6877130a49a79b04428e623e93e44266605a03
David <maxlex9854@gmail.com>
2023-03-12 00:40:37 -0300

    .

---
+++ scripts/cadastroScript.js
@@ -0,0 +1,31 @@
+function formatarCPF(input) {
+    // Remove todos os caracteres que não são números
+    input.value = input.value.replace(/\D/g, '');
+ 
+    // Formata o CPF com a máscara XXX.XXX.XXX-XX
+    input.value = input.value.replace(/(\d{3})(\d{3})(\d{3})(\d{2})/, '$1.$2.$3-$4');
+}
+
+function formatarCEP(input) {
+    // Remove todos os caracteres que não são números
+    input.value = input.value.replace(/\D/g, '');
+ 
+    // Formata o CEP com a máscara XXX.XXX.XXX-XX
+    input.value = input.value.replace(/(\d{5})(\d{3})/, '$1-$2');
+}
+
+function formatarCEL(input) {
+    // Remove todos os caracteres que não são números
+    input.value = input.value.replace(/\D/g, '');
+ 
+    // Formata o Telefone com a máscara (XX)XXXXX-XXXX
+    input.value = input.value.replace(/(\d{2})(\d{5})(\d{4})/, '($1)$2-$3');
+}
+
+function formatarData(input) {
+    // Remove todos os caracteres que não são números
+    input.value = input.value.replace(/\D/g, '');
+ 
+    // Formata o Telefone com a máscara XX/XX/XXXX
+    input.value = input.value.replace(/(\d{2})(\d{2})(\d{4})/, '$1/$2/$3');
+}
---
+++ scripts/homeScipt.js
@@ -0,0 +1,12 @@
+function filterProduct(value) {
+    //Button class code
+    let buttons = document.querySelectorAll(".button-value");
+    buttons.forEach((button) => {
+      //check if value equals innerText
+      if (value.toUpperCase() == button.innerText.toUpperCase()) {
+        button.classList.add("active");
+      } else {
+        button.classList.remove("active");
+      }
+    });
+}
