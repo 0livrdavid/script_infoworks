02d132134792611c1e15067161eb90abfee2a6c8
David <maxlex9854@gmail.com>
2023-03-26 00:59:43 -0300

    .

--- app/login_cadastro/index.php
+++ app/login_cadastro/index.php
@@ -6,33 +6,7 @@ require_once '../../layout/start.php';
 ?>
 
 <div class="div-login-cadastrar">
-    <div class="logo-login-cadastro">
-        <div class="logo-login-cadastro">
-            <img src="<?php echo URL_BASE_ASSETS_PICTURES; ?>InfoWorks_logo.png" alt="" srcset="">
-        </div>
-    </div>
-
-    <div class="center">
-        <h1>Entrar</h1>
-        <form method="post">
-            <div class="txt_field">
-                <input type="text" required oninput="formatarCPF(this)" minlength="11" maxlength="14">
-                <span></span>
-                <label>CPF</label>
-            </div>
-            <div class="txt_field">
-                <input type="password" required>
-                <span></span>
-                <label>Senha</label>
-            </div>
-            <div class="pass">Esqueceu sua senha?</div>
-            <input type="submit" value="Entrar">
-            <div class="signup_link">
-                Ainda não possui uma conta? <a href="./cadastro.php">Cadastrar</a>.
-                <p><a href="../dashboard/">Voltar</a> à Página Inicial.</p>
-            </div>
-        </form>
-    </div>
+    <?php include "./login.php" ?>
 </div>
 
 
--- app/login_cadastro/login.php
+++ app/login_cadastro/login.php
@@ -1,42 +1,27 @@
-<!DOCTYPE html>
-<!-- Coding By CodingNepal - youtube.com/codingnepal -->
-<html lang="pt-br" dir="ltr">
-  <head>
-    <meta charset="utf-8">
-    <title>InfoWorks - Entre na plataforma</title>
-    <link rel="stylesheet" href="styles/loginStyle.css">
-    <script src="scripts/cadastroScript.js"></script>
-    <link rel="icon" type="image/x-icon" href="pictures/InfoWorks_logo_fundo.ico">
-  </head>
-  <body>
-
-    <div class="logo">
-      <div class="img_logo">
-         <img src="pictures/InfoWorks_logo.png" alt="" srcset="">
-      </div>
+<div class="logo-login-cadastro">
+    <div class="logo-login-cadastro">
+        <img src="<?php echo URL_BASE_ASSETS_PICTURES; ?>InfoWorks_logo.png" alt="" srcset="">
     </div>
+</div>
 
-    <div class="center">
-      <h1>Entrar</h1>
-      <form method="post">
+<div class="center">
+    <h1>Entrar</h1>
+    <form method="post">
         <div class="txt_field">
-          <input type="text" required oninput="formatarCPF(this)" minlength="11" maxlength="14">
-          <span></span>
-          <label>CPF</label>
+            <input type="text" required oninput="formatarCPF(this)" minlength="11" maxlength="14">
+            <span></span>
+            <label>CPF</label>
         </div>
         <div class="txt_field">
-          <input type="password" required>
-          <span></span>
-          <label>Senha</label>
+            <input type="password" required>
+            <span></span>
+            <label>Senha</label>
         </div>
         <div class="pass">Esqueceu sua senha?</div>
         <input type="submit" value="Entrar">
         <div class="signup_link">
-          Ainda não possui uma conta? <a href="cadastro.html">Cadastrar</a>.
-          <p><a href="index.html">Voltar</a> à Página Inicial.</p>
+            Ainda não possui uma conta? <a href="./cadastro.php">Cadastrar</a>.
+            <p><a href="../dashboard/">Voltar</a> à Página Inicial.</p>
         </div>
-      </form>
-    </div>
-    
-  </body>
-</html>
+    </form>
+</div>
