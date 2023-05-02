e856cc20f02266a0f53c87152b6256df034b9450
David <maxlex9854@gmail.com>
2023-05-02 11:04:19 -0300

    .

---
+++ app/perfil/footer_user.php
@@ -0,0 +1,5 @@
+<form method="post">
+    <a href="../dashboard/"> <button class="botaoPe2" type="submit">Deslogar</button></a>
+    <input class="botaoPe" type="submit" value="Adicionar Serviço">
+    <input class="botaoPe" type="submit" value="Salvar">
+</form>
---
+++ app/perfil/form_user.php
@@ -0,0 +1,118 @@
+<div class="section">
+    <div class="dados-usuarios-titulo">
+        <h3>Dados do Usuário</h3>
+    </div>
+
+    <div class="dados-usuario">
+        <div class="dados-usuario-form">
+            <h4>Nome completo:</h4>
+            <input type="text" value="Josimar Ferreira" disabled>
+            <h4>CPF:</h4>
+            <input type="text" value="123.456.789-90" disabled>
+            <h4>Data de nascimento:</h4>
+            <input type="text" value="12/03/1997" disabled>
+            <h4>E-mail:</h4>
+            <input type="text" value="josimar.ferreira@gmail.com" disabled>
+        </div>
+        <div class="dados-usuario-foto">
+            <h3 id="txtFotoPerfil">Foto de Perfil</h3>
+            <img src="../../src/pictures/pessoa.jfif" alt="Perfil">
+            <input id="botaoFoto" type="button" value="Carregar foto">
+        </div>
+    </div>
+</div>
+
+
+<div class="section">
+    <div class="dados-conta-titulo">
+        <h3>Dados Pessoais</h3>
+    </div>
+    <div class="dados-conta">
+        <h4>Fale sobre você:</h4>
+        <textarea class="conta-input" maxlength="250"> </textarea>
+        <h4>Formação:</h4>
+        <textarea class="conta-input" maxlength="250"> </textarea>
+    </div>
+</div>
+
+
+<div class="section">
+    <div class="dados-endereco-titulo">
+        <h3>Dados de Endereço</h3>
+    </div>
+    <div class="dados-endereco">
+        <h4>CEP:</h4>
+        <input type="text">
+        <div class="sectionEnderecoP1">
+            <div class="SEP1">
+                <h4>Rua:</h4>
+                <input type="text">
+            </div>
+            <div class="SEP2">
+                <h4>Número:</h4>
+                <input type="text">
+            </div>
+        </div>
+
+        <div class="sectionEnderecoP2">
+            <div class="SEP3">
+                <h4>Cidade:</h4>
+                <input type="text">
+
+            </div>
+            <div class="SEP4">
+                <h4>Estado:</h4>
+                <select id="estado" name="estado">
+                    <option value="AC">Acre</option>
+                    <option value="AL">Alagoas</option>
+                    <option value="AP">Amapá</option>
+                    <option value="AM">Amazonas</option>
+                    <option value="BA">Bahia</option>
+                    <option value="CE">Ceará</option>
+                    <option value="DF">Distrito Federal</option>
+                    <option value="ES">Espírito Santo</option>
+                    <option value="GO">Goiás</option>
+                    <option value="MA">Maranhão</option>
+                    <option value="MT">Mato Grosso</option>
+                    <option value="MS">Mato Grosso do Sul</option>
+                    <option value="MG">Minas Gerais</option>
+                    <option value="PA">Pará</option>
+                    <option value="PB">Paraíba</option>
+                    <option value="PR">Paraná</option>
+                    <option value="PE">Pernambuco</option>
+                    <option value="PI">Piauí</option>
+                    <option value="RJ">Rio de Janeiro</option>
+                    <option value="RN">Rio Grande do Norte</option>
+                    <option value="RS">Rio Grande do Sul</option>
+                    <option value="RO">Rondônia</option>
+                    <option value="RR">Roraima</option>
+                    <option value="SC">Santa Catarina</option>
+                    <option value="SP">São Paulo</option>
+                    <option value="SE">Sergipe</option>
+                    <option value="TO">Tocantins</option>
+                    <option value="EX">Estrangeiro</option>
+                </select>
+            </div>
+        </div>
+        <!-- INPUT SELECT -->
+        <h4>Bairro:</h4>
+        <input type="text">
+    </div>
+</div>
+
+
+<div class="section">
+    <div class="dados-contato-titulo">
+        <h3>Dados de Contato</h3>
+    </div>
+    <div class="dados-contato">
+        <h4>Telefone:</h4>
+        <input type="text">
+        <h4>Whatsapp:</h4>
+        <input type="text">
+        <h4>Instagram:</h4>
+        <input type="text">
+        <h4>Facebook:</h4>
+        <input type="text">
+    </div>
+</div>
